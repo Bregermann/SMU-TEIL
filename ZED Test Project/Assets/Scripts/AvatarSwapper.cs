ebef18ecbc460ea8b25dd5119deb62ea35718e19
Bregermann <bregermann@gmail.com>
2024-04-12 11:59:01 -0400

    Updaed Dance Studio Scene
    Now has the ability to cycle between avatars with the disclaimer that the person needs to exit and re-enter the screen for it to change the avatar at the moment. Press L on the keyboard to cycle avatars

---
+++ ZED Test Project/Assets/Scripts/AvatarSwapper.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarSwapper : MonoBehaviour
+{
+    public ZEDBodyTrackingManager bodyTrackingManager;
+    public GameObject[] avatars;
+    private int currentIndex = 0;
+    public Material[] materials;
+    private int materialIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.L))
+        {
+            CycleToNextAvatar();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            CycleToNextMaterial();
+        }
+    }
+    void CycleToNextAvatar()
+    {
+        bodyTrackingManager.avatar.SetActive(false);
+        currentIndex++;
+        if(currentIndex >= avatars.Length)
+        {
+            currentIndex = 0;
+        }
+        bodyTrackingManager.avatar = avatars[currentIndex];
+        bodyTrackingManager.avatar.SetActive(true);
+
+    }
+    void CycleToNextMaterial()
+    {
+        materialIndex++;
+        if(materialIndex >= materials.Length)
+        {
+            materialIndex = 0;
+        }
+        bodyTrackingManager.skeletonBaseMaterial = materials[materialIndex];
+    }
+}
