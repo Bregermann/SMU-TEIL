11293f5c3a50ec99d53dbef1cadd818cd367367a
Bregermann <bregermann@gmail.com>
2024-05-03 14:07:55 -0400

    Added svo2 support, svo realtime playback enabled, Dance Studio UI buttons moved

--- ZED Test Project/Assets/Scripts/SVOPlayer.cs
+++ ZED Test Project/Assets/Scripts/SVOPlayer.cs
@@ -2,20 +2,28 @@ using UnityEngine;
 using UnityEngine.UI;
 using SFB;  // Standalone File Browser namespace
 using sl;  // ZED SDK namespace
+using System.Windows.Forms;
+using UnityEngine.UIElements;
 
 public class SVOPlayer : MonoBehaviour
 {
-    //public Button openSVOButton;  // UI button to open file dialog
+    public UnityEngine.UI.Button openSVOButton;  // UI button to open file dialog
     public ZEDManager zedManager;  // ZED Manager for handling camera/SVO operations
 
     void Start()
     {
+        openSVOButton.onClick.AddListener(PlaySelectedSVO);
+    }
 
+    void PlaySelectedSVO()
+    {
+        OpenFileDialog();
+        ResetPlayback();
     }
     public void OpenFileDialog()
     {
         // Open file dialog and restrict to SVO files
-        var filters = new[] { new ExtensionFilter("SVO Files", "svo") };
+        var filters = new[] { new ExtensionFilter("SVO Files", "svo", "svo2") };
         var paths = StandaloneFileBrowser.OpenFilePanel("Select an SVO File", "", filters, false);
 
         if (paths.Length > 0)
